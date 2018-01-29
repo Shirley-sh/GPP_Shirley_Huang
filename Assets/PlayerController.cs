b06405e9b12054effd4567262974cb49eaf3ebc6
Shirley Huang <shirley.huang@nyu.edu>
2018-01-29 00:36:03 -0500

    1st commit
    week1

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float moveSpeed;
+    public float rotationSpeed;
+    public float shootForce;
+    public GameObject bullet;
+    Rigidbody2D rd;
+    Vector2 AxisInput;
+
+
+    void Start(){
+        rd = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update(){
+        if (Input.GetMouseButtonDown(0))
+            ShootBullet();
+    
+    }
+
+    void FixedUpdate(){
+        Move();
+        MouseLook();
+
+
+
+    }
+
+    void MouseLook(){
+        Vector3 lookPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        lookPos = lookPos - transform.position;
+        float angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), Time.deltaTime * rotationSpeed);
+        //transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        rd.angularVelocity = 0;
+    }
+
+    void Move(){
+        AxisInput = (new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+        rd.AddForce(AxisInput * moveSpeed);
+        //rd.velocity = new Vector2(1,0);
+    }
+
+    void ShootBullet(){
+        GameObject bulletInstance = Instantiate(bullet, transform.position, transform.rotation);
+        bulletInstance.GetComponent<Rigidbody2D>().AddForce(transform.right * shootForce,ForceMode2D.Impulse);
+        Debug.Log(transform.forward);
+        //bulletInstance.GetComponent<Rigidbody2D>().AddForce(new Vector2(1,0));
+
+    }
+}
